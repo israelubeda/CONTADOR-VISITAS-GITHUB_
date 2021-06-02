4174cac2dbee6cb661aa6b0357c874c10b336b21
Key Cuevas Melgarejo <38257705+KeyCuevasMelgarejo@users.noreply.github.com>
2021-06-02 13:42:27 -0500

    Update conexion.php

--- src/conexion.php
+++ src/conexion.php
@@ -1,8 +1,8 @@
 <?php
-    $hostname_db = "remotemysql.com";
-    $database_db = "XVh7j3cTff";
-    $username_db = "XVh7j3cTff";
-    $password_db = "JTZ5F3WYc5";
+    $hostname_db = "hostname_db";
+    $database_db = "database_db";
+    $username_db = "username_db";
+    $password_db = "password_db";
 
     $con = mysqli_connect($hostname_db,$username_db,$password_db,$database_db);
     if (mysqli_connect_errno()){
